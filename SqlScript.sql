0a124811705b216c9053946d4139d37933f4b5c3
Марк Евгеньевич <belebred@yandex.ru>
2023-07-31 19:58:59 +0300

    Add second task

---
+++ SqlScript.sql
@@ -0,0 +1,6 @@
+﻿SELECT Name AS 'Продукт', Cathegory AS 'Категория'
+FROM Product
+LEFT JOIN ProductCathegory
+	ON (Product.ID = ProductCathegory.ProductID)
+LEFT JOIN Cathegory
+	ON (Cathegory.ID = ProductCathegory.CathegoryID)
